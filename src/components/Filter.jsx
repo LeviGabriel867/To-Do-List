6652d4992ff535d6741a8081c59c71ebf0a02f14
LeviGabriel867 <levideveloper373@gmail.com>
2024-06-27 21:21:32 -0300

    To do list que será usado em projetos futuros - LifeFit

---
+++ src/components/Filter.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Filter = ({filter, setFilter,setSort}) => {
+  return (
+    <div className='filter'>
+      <h2>Filtrar:</h2>
+      <div className='filter-options'>
+        <div>
+            <p>Status:</p>
+            <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+                <option value="All">Todas</option>
+                <option value="Completed">Completas</option>
+                <option value="Incomplete">Incompletas</option>
+            </select>
+        </div>
+        <div>
+            <p>Ordem alfabética</p>
+            <button onClick={()=>setSort("Ascendente")}>Ascendente</button>
+            <button onClick={()=>setSort("Descendente")}>Descendente</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Filter
